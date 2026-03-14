f29c4aa6cdf2500c9850f7b9970e5a518135b81f
Xige Michael Chen <mxchen2001@gmail.com>
2026-03-13 22:34:48 -0500

    base

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/compensair/:path*",
+        destination: "https://compensair.vercel.app/compensair/:path*",
+      },
+      {
+        source: "/cc-fyi/:path*",
+        destination: "https://cc-fyi.vercel.app/cc-fyi/:path*",
+      }
+    ];
+  },
 };
 
 export default nextConfig;
