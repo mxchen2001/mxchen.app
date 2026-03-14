10da530cce129223ffe220effd5540e9f59a8b6e
Xige Michael Chen <mxchen2001@gmail.com>
2026-03-13 23:24:07 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/compensair/:path*",
-        destination: "https://compensair.vercel.app/compensair/:path*",
+        destination: "https://compensair.vercel.app/:path*",
       },
       {
         source: "/cc-fyi/:path*",
-        destination: "https://cc-fyi.vercel.app/cc-fyi/:path*",
+        destination: "https://cc-fyi.vercel.app/:path*",
       }
     ];
   },
